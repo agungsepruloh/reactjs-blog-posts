c545cac85d0b843bc96f3810212003d29c379752
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-17 23:54:51 +0700

    Fetch Singular Records

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,6 +10,15 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data
+  });
+};
+
 // export const fetchPosts = () => {
 // Bad approach !!!
 // It's will got an error
