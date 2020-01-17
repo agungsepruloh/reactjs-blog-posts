b9e7a02b525438dd38679981c885787247c08b8a
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-17 07:04:31 +0700

    Make a Request From an Action Creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
-export const fetchPosts = () => {
+import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+export const fetchPosts = async () => {
+  // Bad approach !!!
+  // It's will got an error
+  const response = await jsonPlaceHolder.get("/posts");
+
   return {
-    type: "FETCH_POSTS"
+    type: "FETCH_POSTS",
+    payload: response
   };
 };
