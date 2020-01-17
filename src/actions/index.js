e874b89f2841a93a2e36809af8b58779d10a9b6b
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-18 05:16:46 +0700

    Memoizing Functions and One Time Memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+import _ from "lodash";
 
 // Absolutely get the same result (good result)
 export const fetchPosts = () => async dispatch => {
@@ -10,14 +11,17 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = id => async dispatch => {
+// absolutely get the same results
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceHolder.get(`/users/${id}`);
 
-  dispatch({
-    type: "FETCH_USER",
-    payload: response.data
-  });
-};
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
 
 // export const fetchPosts = () => {
 // Bad approach !!!
